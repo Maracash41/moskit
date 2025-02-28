7999828f8f28c1a127d610099a6fc7b9e4155b20
Maracash41 <9531945@gmail.com>
2025-02-28 13:39:04 +0300

    feat: add second bannner and rewrite banner component

--- mosquito/src/components/Banner/Banner.tsx
+++ mosquito/src/components/Banner/Banner.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import classes from "./banner.module.css";
 import bannerImg from "../../assets/banner_img.png";
 
-const Banner: React.FC = () => {
+interface IBanner {
+  image: string;
+  altText: string;
+  text: string;
+}
+
+const Banner: React.FC<IBanner> = ({ image, altText, text }) => {
   return (
     <section className={classes.banner}>
       <div className={classes.container}>
         <div className={classes.bannerContent}>
-          <div className={classes.bannerText}>
-            У нас так же можно заменить фурнитуру, уплотнительные резинки и
-            настроить окна
-          </div>
+          <p className={classes.bannerText}>{text}</p>
           <div className={classes.bannerImageContainer}>
-            <img src={bannerImg} alt="баннер рисунок" />
+            <img src={image} alt={altText} />
           </div>
         </div>
       </div>
--- mosquito/src/components/Main/Main.tsx
+++ mosquito/src/components/Main/Main.tsx
@@ -4,15 +4,31 @@ import Banner from "../Banner/Banner";
 import Nets from "../Nets/Nets";
 import NetTypes from "../NetTypes/NetTypes";
 
+import furnituraImg from "./img/banner_furnitura.png";
+import ramkiImg from "./img/banner_ramki.jpg";
+
 const Main: React.FC = () => {
   return (
     <>
       <main className={classes.main}>
         <div className={classes.mainContent}>
-          <Banner />
+          <Banner
+            image={furnituraImg}
+            altText={"фурнитура"}
+            text={
+              "У нас так же можно заменить фурнитуру, уплотнительные резинки и настроить окна"
+            }
+          />
           <div className={classes.container}>
             <Nets />
             <NetTypes />
+            <Banner
+              image={ramkiImg}
+              altText={"Окраска рам сеток"}
+              text={
+                "Мы предлагаем 3 стандартных цвета, а так же можем окрасить в любой цвет "
+              }
+            />
           </div>
         </div>
       </main>
