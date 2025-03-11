d88db9e40de534c00523b4690cb5f1d429df2336
Maracash41 <9531945@gmail.com>
2025-03-11 13:59:03 +0300

    feat: add ModalProvider

--- mosquito/src/components/utils/ModalProvider.tsx
+++ mosquito/src/components/utils/ModalProvider.tsx
@@ -1,5 +1,47 @@
-import React, { createContext } from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 
-const ModalContext = createContext();
+interface ModalContextType {
+  isOpenModal: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+  phoneNumber: string;
+  handlePhoneNumberChange: (number: string) => void;
+}
 
-export const ModalProvider = () => {};
+interface ModalProviderProps {
+  children: ReactNode;
+}
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const openModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const handlePhoneNumberChange = (number: string) => {
+    setPhoneNumber(number);
+  };
+
+  return (
+    <ModalContext.Provider
+      value={{
+        isOpenModal,
+        openModal,
+        closeModal,
+        phoneNumber,
+        handlePhoneNumberChange,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
+export const useModal = useContext(ModalContext);
