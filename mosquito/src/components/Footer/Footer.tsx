d260493a30806b07ac066e9606a6d3811ef74fb0
Maracash41 <9531945@gmail.com>
2025-03-21 16:37:24 +0300

    feat: add test build

--- mosquito/src/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./footer.module.css";
-
-const Footer: React.FC = () => {
-  return <footer className={classes.footer}></footer>;
-};
-
-export default Footer;
